06dac4b2940c0b275baa86eb574f7b088a44e0bd
CatMoons <sheogor2@gmail.com>
2024-10-09 22:58:05 +0300

    code editor

--- src/processing.py
+++ src/processing.py
@@ -1,21 +1,26 @@
-def filter_by_state(list_dictionary: list, state: str = 'EXECUTED') -> list:
-    list_executed = []
-    lrst_canceled = []
+def filter_by_state(list_dictionary: list, state: str = "EXECUTED") -> list:
+    """
+    Функция принимает список словарей и опционально значение для ключа
+    state (по умолчанию 'EXECUTED'). Возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению.
+    """
+    list_executed: list = []
+    list_canceled: list = []
     for key in list_dictionary:
-        if key.get('state') == state:
+        if key.get("state") == state:
             list_executed.append(key)
         else:
-            lrst_canceled.append(key)
-    return f"{list_executed}\n{lrst_canceled}"
+            list_canceled.append(key)
 
 
-
+    return f"{list_executed}\n{list_canceled}"
 
 
 def sort_by_date(list_dictionary: list, sort_order: bool = True) -> list:
-    sort_date = sorted(list_dictionary, key=lambda x: x['date'], reverse=sort_order)
+    """
+    Функция принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате (date).
+    """
+    sort_date = sorted(list_dictionary, key=lambda x: x["date"], reverse=sort_order)
     return sort_date
-
-
-
-
