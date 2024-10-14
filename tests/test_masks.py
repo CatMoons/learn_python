cfcbe4ff86672f6acfb995f79bd334d78fe04605
CatMoons <sheogor2@gmail.com>
2024-10-15 00:22:41 +0300

    create test_masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -10,7 +10,8 @@ from src.masks import get_mask_card_number, get_mask_account
         ("7158300734726758", "7158 30** **** 6758"),
         ("6831982476737658", "6831 98** **** 7658"),
         ("123456", "1234 56** **** "),
-        ("1234567890123456789012", "1234 56** **** 3456789012")
+        ("1234567890123456789012", "1234 56** **** 3456789012"),
+        ("", " ** **** ")
     ],
 )
 def test_get_mask_card_number(type_and_number, expected):
@@ -19,7 +20,8 @@ def test_get_mask_card_number(type_and_number, expected):
 
 @pytest.mark.parametrize(
     "type_and_number, expected",
-    [("73654108430135874305", "**4305"), ("73654108430135877892", "**7892"), ("73654108430135871005", "**1005")],
+    [("73654108430135874305", "**4305"), ("73654108430135877892", "**7892"), ("73654214108430135871005", "**1005"),
+     ("12345", "**2345"), ("skypro", "**ypro")],
 )
 def test_get_mask_account(type_and_number, expected):
     assert get_mask_account(type_and_number) == expected
