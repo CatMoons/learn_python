2b314d23b628b4e131c5421c01119694aa05f11a
CatMoons <sheogor2@gmail.com>
2024-10-15 00:07:41 +0300

    create test_masks

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(number_card: str) -> str:
     """Функция которая принимает на вход номер карты и возвращает ее маску."""
 
-    return f"{number_card[:4]} {number_card[5:7]}** **** {number_card[12:]}"
+    return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
---
+++ tests/test_masks.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize(
+    "type_and_number, expected",
+    [
+        ("1596837868705199", "1596 83** **** 5199"),
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("6831982476737658", "6831 98** **** 7658"),
+    ],
+)
+def test_get_mask_card_number(type_and_number, expected):
+    assert get_mask_card_number(type_and_number) == expected
+
+
+@pytest.mark.parametrize(
+    "type_and_number, expected",
+    [("73654108430135874305", "**4305"), ("73654108430135877892", "**7892"), ("73654108430135871005", "**1005")],
+)
+def test_get_mask_account(type_and_number, expected):
+    assert get_mask_account(type_and_number) == expected
