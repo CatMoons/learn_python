f9601764a35b0e00105da4791518234ec5eef4c4
CatMoons <sheogor2@gmail.com>
2024-10-17 21:54:59 +0300

    fix test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -24,9 +24,9 @@ def test_filter_by_state(list_dict: list, state: str = "EXECUTED") -> None:
             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         ]
-    elif state == "1234":
+    elif state == 1234:
         assert filter_by_state(list_dict) == [
-            {"id": 615064591, "state": "1234", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 615064591, "state": 1234, "date": "2018-10-14T08:21:33.419441"},
         ]
     elif state == "RR":
         assert filter_by_state(list_dict) == [
@@ -37,24 +37,24 @@ def test_filter_by_state(list_dict: list, state: str = "EXECUTED") -> None:
 
 
 @pytest.fixture
-def list_dicti() -> list:
+def list_dictio() -> list:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2023-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2012-10-14T08:21:33.419441"},
         {"id": 615064591, "state": "RR", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 615064591, "state": "1234", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 615064591, "state": 1234, "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
 def test_sort_by_date(list_dictio: list, sort_order: bool = True) -> None:
     if sort_order:
         assert sort_by_date(list_dictio) == [
-            {"id": 939719570, "state": "EXECUTED", "date": "2023-06-30T02:08:58.425572"},
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 615064591, "state": "RR", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 615064591, "state": "1234", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2012-10-14T08:21:33.419441"},
+             {'id': 939719570, 'state': 'EXECUTED', 'date': '2023-06-30T02:08:58.425572'},
+             {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+             {'id': 615064591, 'state': 'RR', 'date': '2018-10-14T08:21:33.419441'},
+             {'id': 615064591, 'state': 1234, 'date': '2018-10-14T08:21:33.419441'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+             {'id': 615064591, 'state': 'CANCELED', 'date': '2012-10-14T08:21:33.419441'}
         ]
