7a00e7c1eb8418b306ad38c874e0f2056b3409e1
CatMoons <sheogor2@gmail.com>
2024-11-01 22:32:57 +0300

    create utils.py

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+
+def get_info_transactions_json(file_path):
+
+    data_empty_list: list = []
+
+    if not os.path.exists(file_path):
+        return []
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        try:
+            operation = json.load(f)
+            if len(operation) == 0 or type(operation) != list:
+                return data_empty_list
+            else:
+                return operation
+        except json.decoder.JSONDecodeError:
+            return data_empty_list
+
+
+if __name__ == '__main__':
+    data = get_info_transactions_json("E:\pycharm_project\widget_personal_cabinet\data\operations.json")
+    print(data)
+
+
