1022fdb0beae51da1d022ae10a38204e31bde605
CatMoons <sheogor2@gmail.com>
2024-11-01 22:33:16 +0300

    create external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,46 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+from json import JSONDecodeError
+
+def convert_sum(file_path):
+    load_dotenv(".env")
+    api_key = os.getenv("API_KEY")
+    headers = {"api-key": api_key}
+    amount_list = []
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        try:
+            operations = json.load(f)
+            for transaction in operations:
+                operation_amount = transaction.get("operationAmount")
+                if operation_amount is None:
+                    continue
+
+                code_from_transaction = operation_amount["currency"]["code"]
+                amount_from_transaction = float(operation_amount["amount"])
+
+                if code_from_transaction == "RUB":
+                    amount_list.append(amount_from_transaction)
+                elif code_from_transaction in ["USD", "EUR"]:
+
+                    url = f"https://api.apilayer.com/currency_data/convert?base=USD&symbols=EUR,GBP,JPY&amount=5"
+                    params = {
+                        "from": code_from_transaction,
+                        "to": "RUB",
+                        "amount": amount_from_transaction
+                    }
+                    response = requests.get(url, headers=headers, params=params)
+                    if response.ok:
+                        conversion_result = response.json()
+                        converted_amount = conversion_result.get("result", 0)
+                        amount_list.append(float(converted_amount))
+
+            return amount_list
+        except JSONDecodeError:
+            return []
+
+if __name__ == '__main__':
+    data = convert_sum("E:\\\\pycharm_project\\\\widget_personal_cabinet\\\\data\\\\operations.json")
+    print(data)
